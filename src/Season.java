4236348b13813ee8168eb93a0d22f0b5d904a9bd
eln008 <140242882+eln008@users.noreply.github.com>
2023-08-07 22:43:01 +0600

    Seasons

--- src/Season.java
+++ src/Season.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Season {
+import java.util.Scanner;
+
+public class Season {
+
+
+
+    public void findSeason(int monthNumber) {
+       if (monthNumber >= 1 && monthNumber <= 12) {
+           String season;
+           switch (monthNumber) {
+               case 12:
+               case 1:
+               case 2:
+                   season = "зима";
+                   break;
+               case 3:
+               case 4:
+               case 5:
+                   season = "весна";
+                   break;
+               case 6:
+               case 7:
+               case 8:
+                   season = "лето";
+                   break;
+               default:
+                   season = "осень";
+                   break;
+           }
+           System.out.println("Сезон: " + season);
+       } else {
+           System.out.println("Введенное значение не является порядковым номером месяца.");
+       }
+
+   }
 }
+
